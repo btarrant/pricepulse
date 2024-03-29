998d9cac0889f353c419ba21b99392af1a2fe95d
Brandon Tarrant <brandontarrant04@gmail.com>
2024-03-28 23:33:15 -0400

    Add logo to Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="w-full">
+      <nav className="nav">
+        <Link href="/" className="flex items-center gap-1">
+          <Image src="/assets/icons/logo.svg" width={27} height={27} alt="logo" />
+          <p className="nav-logo">
+            Price<span className="text-primary">Wise</span>
+          </p>
+        </Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
