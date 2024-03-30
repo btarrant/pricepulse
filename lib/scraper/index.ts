b442fc9cc56d1ed728eee14c5848b0eae4f879f6
Brandon Tarrant <brandontarrant04@gmail.com>
2024-03-29 21:09:27 -0400

    Built function to extract prices from Amazon

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
+import { extractPrice } from "../utils";
 
 export async function scrapeAmazonProduct(url: string) {
   if (!url) return;
@@ -27,8 +28,19 @@ export async function scrapeAmazonProduct(url: string) {
 
     // Extract product title
     const title = $("#productTitle").text().trim();
+    const currentPrice = extractPrice(
+      $(`.priceToPay span.a-price-whole`)
+      //   $(`a.size.base.a-color-price`),
+      //   $(`.a-button-selected .a-color-base`),
+    );
 
-    console.log({ title });
+    const originalPrice = extractPrice(
+      $(`#priceblock_ourprice`),
+      $(`.a-price.a-text-price span.a-offscreen`),
+      $(`a-price.a-text-price`)
+    );
+
+    console.log({ title, currentPrice });
   } catch (error: any) {
     throw new Error(`Failed to scrape product: ${error.message}`);
   }
