780d2149d8f3460ec4cdc8f0e4d4cd4ffb25445e
btarrant <brandontarrant04@gmail.com>
2025-06-15 10:20:04 -0400

    Update the Product type to match the DB

--- types/index.ts
+++ types/index.ts
@@ -6,25 +6,27 @@ export type User = {
   email: string;
 };
 
-export type Product = {
-  _id?: string;
+export interface Product {
+  _id: string;
   url: string;
   currency: string;
   image: string;
-  title: string;
+  title?: string;
   currentPrice: number;
   originalPrice: number;
-  priceHistory: PriceHistoryItem[] | [];
-  highestPrice: number;
-  lowestPrice: number;
-  averagePrice: number;
-  discountRate: number;
-  description: string;
-  category: string;
-  reviewsCount: number;
-  stars: number;
-  isOutOfStock: Boolean;
-  users?: User[];
+  priceHistory: { price: number; date: string }[];
+  lowestPrice?: number;
+  highestPrice?: number;
+  averagePrice?: number;
+  discountRate?: number;
+  description?: string;
+  category?: string;
+  reviewsCount?: number;
+  stars?: number;
+  isOutOfStock?: boolean;
+  users: { email: string }[];
+  createdAt?: string;
+  updatedAt?: string;
 };
 
 export type NotificationType = "WELCOME" | "CHANGE_OF_STOCK" | "LOWEST_PRICE" | "THRESHOLD_MET";
